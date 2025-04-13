6c1d0d4fbc42f4bf343a8ce0c1a7148d0b2cccc3
carlos676767 <carlitobr780@yahoo.com>
2025-04-13 14:48:01 -0400

    sobremim remived

--- web/server.js
+++ web/server.js
@@ -2,17 +2,14 @@ import expressApi from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import routerApi from "./routers/routers.js";
-import env from "dotenv";
+
 
 class Express {
   static api = expressApi();
   
-  static envConfig(){
-    return env.config();
-  }
- 
+
   static configExpress() {
-    Express.envConfig()
+
     this.api.use(cors());
     this.api.use(bodyParser.json());
     this.api.use(routerApi)
