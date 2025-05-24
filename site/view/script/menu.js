05bee911eea7905b732068b4837aa19957343056
carlos676767 <carlitobr780@yahoo.com>
2025-05-24 12:49:32 -0400

    animacao de entrada no home portfolio ok.

---
+++ site/view/script/menu.js
@@ -0,0 +1,32 @@
+class Header {
+    static open() {
+        const menuToggle = document.getElementById("menuToggle");
+        const mobileMenu = document.getElementById("mobileMenu");
+
+        menuToggle.addEventListener("click", () => {
+            mobileMenu.classList.toggle("hidden");
+        });
+    }
+
+
+    static get getBar() {
+        return document.getElementById('navbar');
+    }
+
+
+    static eventBar() {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 10) {
+                return this.getBar.classList.add('shadow-2xl', 'bg-opacity-90', 'backdrop-blur-md');
+
+            }
+            this.getBar.classList.remove('shadow-2xl', 'bg-opacity-90', 'backdrop-blur-md');
+        });
+
+    }
+}
+
+addEventListener(`DOMContentLoaded`, () => {
+    Header.open();
+    Header.eventBar()
+})
